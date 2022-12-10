8a90aecff5dd43ab539d5e2ce2a4f6b80f32b1bf
InsertNameSUP <Insert.NameSUP@gmail.com>
2022-12-10 13:43:11 +1100

    Fix changed directory + Fetch if file not found

--- lua/autorun/client/raw2list.lua
+++ lua/autorun/client/raw2list.lua
@@ -1,20 +1,39 @@
 contents = {}
-raw = file.Read("sup/props_whitelist.dat", "DATA")
-raw = raw:gsub("'", "")
-raw = raw:gsub("}", "")
-raw = raw:gsub("{", "")
-raw = raw:gsub("~", "")
-raw = raw:gsub(" ", "")
-raw = raw:sub(1, -2)
-parsed = string.Split(raw, ";")
+raw = file.Read("sup/rp/props_whitelist.dat", "DATA")
 
-for _, model in ipairs(parsed) do
-    print("... ", model)
-    table.insert( contents, {
-		type = "model",
-		model = model
-	} )
+function createPropList(propList)
+	for _, model in ipairs(propList) do
+		table.insert( contents, {
+			type = "model",
+			model = model
+		} )
+	end
+	spawnmenu.AddPropCategory("SUPAddon", "SUP Whitelist", contents, "icon16/box.png")
 end
 
 
-spawnmenu.AddPropCategory("SUPAddon", "SUP Whitelist", contents, "icon16/box.png")
+if raw == nil then
+	http.Fetch( "https://gmod-api.superiorservers.co/api/darkrp/whitelist",
+		function( body, length, headers, code )
+			createPropList(util.JSONToTable(body))
+		end,
+		function( message )
+			print( message )
+		end,
+		{ 
+			["accept-encoding"] = "gzip, deflate",
+			["accept-language"] = "fr" 
+		}
+	)
+
+else 
+
+	raw = raw:gsub("'", "")
+	raw = raw:gsub("}", "")
+	raw = raw:gsub("{", "")
+	raw = raw:gsub("~", "")
+	raw = raw:gsub(" ", "")
+	raw = raw:sub(1, -2)
+	parsed = string.Split(raw, ";")
+	createPropList(parsed)
+end
